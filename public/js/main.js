1812265b001f490edab5b2ccc69466ededcf5640
michalbrk <brakowski.m@gmail.com>
2018-02-10 19:58:08 +0100

    adding canvas sprites handling logic

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,39 @@ function loadImage(url) {
     }); 
 }
 
+class SpriteSheet {
+    constructor(image, width, height) {
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.tiles = new Map();
+    }
+    
+    define(name, x, y) {
+        const buffer = document.createElement('canvas');
+        buffer.width = this.width;
+        buffer.height = this.height;
+        buffer
+            .getContext('2d')
+            .drawImage(
+                this.image, 
+                x * this.width,
+                y * this.height,
+                this.width,
+                this.height,
+                0,
+                0,
+                this.width,
+                this.height);
+        this.tiles.set(name, buffer);
+    }
+    
+    draw(name, context, x, y) {
+        const buffer = this.tiles.get(name);
+        context.drawImage(buffer, x, y);    
+    }
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
@@ -15,5 +48,9 @@ context.fillRect(0,0,50,50);
 
 loadImage('./img/tiles.png')
 .then(image => {
-      context.drawImage(image, 0, 0);
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.draw('ground', context, 45, 62);
+    
+    context.drawImage(image, 0, 0);
 });
