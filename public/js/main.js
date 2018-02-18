33c02bc0beb0b416eb71796e959b6dc8a1e4e05e
michalbrk <brakowski.m@gmail.com>
2018-02-18 14:27:52 +0100

    implementing traits to the keys handling logic

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,6 @@ import {loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 import Keyboard from './KeyboardState.js';
 
-const input = new Keyboard();
-input.addMapping(32, keyState => {
-    
-});
-input.listenTo(window);
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
@@ -30,6 +24,17 @@ Promise.all([
     const gravity = 2000;
     mario.pos.set(64,180);
     mario.vel.set(200,-600);
+    
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if(keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    });
+    input.listenTo(window);
     
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
