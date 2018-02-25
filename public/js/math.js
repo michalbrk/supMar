912bfb2c23ff8a2e9e2e33b3dc0eba5c200d50bd
michalbrk <brakowski.m@gmail.com>
2018-02-25 17:12:39 +0100

    refractoring the tile logic into other module

--- public/js/math.js
+++ public/js/math.js
@@ -3,6 +3,14 @@ export class Matrix {
         this.grid = [];
     }
     
+    forEach(callback) {
+        this.grid.forEach((column, x) => {
+            column.forEach((value, y) => {
+                callback(value, x, y);
+            });
+        });
+    }
+    
     set(x,y, value) {
         if(!this.grid[x]) {
             this.grid[x] = [];
