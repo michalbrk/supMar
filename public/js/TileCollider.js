89902736a9fd4dfcb5924da9860644c13711847f
michalbrk <brakowski.m@gmail.com>
2018-02-25 17:34:08 +0100

    implementation of the game's collision logic

---
+++ public/js/TileCollider.js
@@ -0,0 +1,15 @@
+class TileResolver {
+    constructor(matrix, tileSize = 16) {
+        this.matrix = matrix;
+    }
+}
+
+export default class tileCollider {
+    constructor(tiles) {
+        this.tiles = tiles;
+    }
+    
+    test(entity) {
+        
+    }
+}
