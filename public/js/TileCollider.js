a8bcfeabc2d148d3aac1404eb643f14af4701214
michalbrk <brakowski.m@gmail.com>
2018-02-26 22:50:24 +0100

    implementing the tile positions/collision logic

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -5,6 +5,24 @@ export default class tileCollider {
         this.tiles = new TileResolver(tileMatrix);
     }
     
+    checkY(entity) {
+        const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
+        if(!match) {
+            return;
+        }
+        
+        if(match.tile.name != 'ground') {
+            return;
+        }
+        
+        if(entity.vel.y > 0) {
+            if(entity.pos.y > match.y1) {
+                entity.pos.y = match.y1;
+                entity.vel.y = 0;
+            }
+        }
+    }
+    
     test(entity) {
 //        const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
 //        if(match) {
