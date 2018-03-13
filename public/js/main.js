8f0f7b631f36d9cc4272ad24a26cb6ce6de22a28
michalbrk <brakowski.m@gmail.com>
2018-03-13 23:08:47 +0100

    implementing the camerLater function to hold the current view

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import Camera from './Camera.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {createCollisionLayer} from './layers.js';
+import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {setupKeyboard} from './input.js';
 import {setupMouseControl} from './debug.js';
 
@@ -19,7 +19,9 @@ Promise.all([
     
     mario.pos.set(64, 64);
 
-    level.comp.layers.push(createCollisionLayer(level));
+    level.comp.layers.push(
+        createCollisionLayer(level),
+        createCameraLayer(camera));
 
     level.entities.add(mario);
 
