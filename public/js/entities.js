89afeec8aa4938570dc69ff0f88a4b46d19a46f7
michalbrk <brakowski.m@gmail.com>
2018-03-20 20:45:20 +0100

    updating the modules with the orginal instance

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,6 +1,7 @@
 import Entity from './Entity.js';
 import Go from './traits/Go.js';
 import Jump from './traits/Jump.js';
+import Velocity from './traits/Velocity.js';
 import {loadMarioSprite} from './sprites.js';
 
 export function createMario() {
@@ -11,6 +12,7 @@ export function createMario() {
 
         mario.addTrait(new Go());
         mario.addTrait(new Jump());
+        //mario.addTrait(new Velocity());
 
         mario.draw = function drawMario(context) {
             sprite.draw('idle', context, 0, 0);
